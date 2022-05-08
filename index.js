0c3f4994e68f6e60857c0a2b5a696e8eae7e1ce7
Asmit <asmitbuddy@gmail.com>
2022-05-08 13:38:22 +0530

    modified index.js

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.listen(process.env.PORT || 3000, ()=>{
-    console.log('Application is started');
-});
+app.set( 'port', ( process.env.PORT || 5000 ));
+
+// Start node server
+app.listen( app.get( 'port' ), function() {
+  console.log( 'Node server is running on port ' + app.get( 'port' ));
+  });
+// app.listen(process.env.PORT || 3000, ()=>{
+//     console.log('Application is started');
+// });
