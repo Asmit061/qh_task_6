e569d8eba7c8cb903e526abb54b61b4be8d745c3
Asmit <asmitbuddy@gmail.com>
2022-05-08 13:18:56 +0530

    final code

--- public/video-player.js
+++ public/video-player.js
@@ -2,24 +2,19 @@ var player = videojs('videoPlayer', {
     controls: true,   
     autoplay: 'muted'
 });
+var currentVideo = 0;
+var videos = new Array("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4","http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"); //("BigBuck.m4v","Video.mp4","BigBuck.m4v","Video2.mp4");
+function nextVideo(){
+    player.src({
+        type: 'video/mp4',
+        src: videos[currentVideo]
+    })
+    player.play();
+    currentVideo=(currentVideo+1)%videos.length;
+};
 var previewPlayer= videojs(document.querySelector('.video-js'));
+player.on('ended',nextVideo);
 previewPlayer.bigPlayButton.on('click', function(){
-    var currentVideo = 0;
-    var videos = new Array("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4","http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"); //("BigBuck.m4v","Video.mp4","BigBuck.m4v","Video2.mp4");
-    
-    function nextVideo(){
-        
-        player.off('ended', nextVideo, false);
-        player.src({
-            type: 'video/mp4',
-            src: videos[currentVideo]
-        })
-        player.play();
-        currentVideo = (currentVideo + 1) % videos.length
-        player.on('ended', nextVideo, false);
-    };
-
-    while(currentVideo <= videos.length){
-        nextVideo();
-    }
+    nextVideo();
 });
+
